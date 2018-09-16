42a6dae523504dbd2b420f666b80f6a675948444
Andro <l22wok@gmail.com>
2018-09-16 09:38:26 +0300

    Add files

---
+++ Language/RU/messages.php
@@ -0,0 +1,19 @@
+<?php
+return array (
+    "test" => "тест(ru)",
+    "Blog" => "Блог",
+    "RF4 Web Map" => "РР4 Веб Карта",
+    "RF4 calc" => "РР4 Калькулятор",
+    "Mosquito Lake" => "оз. Комариное",
+    "Vinding Rivulet" => "р. Вьюнок",
+    "Old Burg Lake" => "оз. Старый Острог",
+    "Kuori Lake" => "оз. Куори",
+    "Bear Lake" => "оз. Медвежье",
+    "Volkhov River" => "р. Волхов",
+    "Sura River" => "р. Сура",
+    "Manual positioning" => "Ручной ввод координат",
+    "Popular coordinates" => "Популярные координаты",
+    "Show" => "Показать",
+    "Let me see!" => "Установить маркер",
+    "If marker bugged, just reload page." => "Если курсор не двигается попробуйте обновить страницу {F5}!"
+);
