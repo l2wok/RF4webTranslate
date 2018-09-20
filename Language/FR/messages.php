12c6955e4c9300652eacf12c65ce77ab4e210e1e
Andro <l22wok@gmail.com>
2018-09-20 12:43:19 +0300

    add FR translate

--- Language/FR/messages.php
+++ Language/FR/messages.php
@@ -1,4 +1,18 @@
 <?php
 
 return array (
+    "RF4 Web Map" => "",
+    "RF4 calc" => "",
+    "Mosquito Lake" => "Lac de Mosquito",
+    "Vinding Rivulet" => "Riviиre de Rivulet",
+    "Old Burg Lake" => "Lac d'Old Burg",
+    "Kuori Lake" => "Lac de Kuori",
+    "Bear Lake" => "Lac de l'ours",
+    "Volkhov River" => "Riviиre de Volkhov",
+    "Sura River" => "Riviиre de Sura",
+    "Manual positioning" => "Ajouter un point manuellement",
+    "Popular coordinates" => "Coordonйes populaires",
+    "Show" => "Montrer",
+    "Let me see!"> "Laisse moi voir ",
+    "If marker bugged, just reload page." => "Si le marqueur est buguer, rafraоchir la page avec f5"
 );
